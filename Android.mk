67bc5eb4df8370e4a38e86a15ffde31b30cc1e29
seba_3567 <44386561+seba3567@users.noreply.github.com>
2020-05-11 18:49:44 -0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@ else ifeq ($(IS_MOTO),720)
 else ifeq ($(IS_MOTO),burton)
      PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/burton.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
-    ifeq ($(IS_MOTOROLA),)
+ifeq ($(IS_MOTOROLA),)
         $(warning "Moto bootanimations: IS_MOTO is undefined, assuming 1080p")
 else
         $(warning "Moto bootanimations: Current bootanimation res is not supported, forcing 1080p (support 1080,720,1440")
